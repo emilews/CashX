a4f426e15cf3f999e8530e904f9939854481a654
Emilio Israel Wong González <emilews.ew@gmail.com>
2019-12-27 21:05:58 -0700

    Removed testNet seed addresses

--- p2p/src/main/java/bisq/network/Socks5SeedOnionDiscovery.java
+++ p2p/src/main/java/bisq/network/Socks5SeedOnionDiscovery.java
@@ -74,13 +74,6 @@ public class Socks5SeedOnionDiscovery implements PeerDiscovery {
         // this list copied from bitcoin-core on 2017-01-19
         //   https://github.com/bitcoin/bitcoin/blob/57b34599b2deb179ff1bd97ffeab91ec9f904d85/contrib/seeds/nodes_test.txt
         return new String[]{
-                "thfsmmn2jbitcoin.onion",
-                "it2pj4f7657g3rhi.onion",
-                "nkf5e6b7pl4jfd4a.onion",
-                "4zhkir2ofl7orfom.onion",
-                "t6xj6wilh4ytvcs7.onion",
-                "i6y6ivorwakd7nw3.onion",
-                "ubqj4rsu3nqtxmtp.onion"
         };
     }
 
